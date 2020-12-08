2024c32ad3a6a4d19e3db2778c71d78e603fee2f
ku78 <60285617+ku78@users.noreply.github.com>
2020-12-08 15:11:30 +0300

    ДЗ1

--- src/main/java/com/geekbrains/shop/entities/OrderItem.java
+++ src/main/java/com/geekbrains/shop/entities/OrderItem.java
@@ -1,9 +1,52 @@
 package com.geekbrains.shop.entities;
 
 
+import lombok.Data;
 
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "order_items")
 public class OrderItem {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Orders order;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "price_per_product")
+    private Integer pricePerProduct;
+
+    public OrderItem(Product product){
+        this.product = product;
+        this.quantity = 1;
+
+    }
+
+    public OrderItem() {
+    }
+
+    public void incrementQuantity() {
+        quantity++;
+    }
+
+    public void decrementQuantity() {
+        quantity--;
+    }
 
 }
--- src/main/java/com/geekbrains/shop/entities/Orders.java
+++ src/main/java/com/geekbrains/shop/entities/Orders.java
@@ -1,8 +1,39 @@
 package com.geekbrains.shop.entities;
 
+import lombok.Data;
+import org.hibernate.annotations.Cascade;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "orders")
 public class Orders {
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "customer_name")
+    private String name;
+
+    @Column(name = "customer_phone")
+    private String phone;
+
+    @Column(name = "customer_address")
+    private String address;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @OneToMany(mappedBy = "order")
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    private List<OrderItem> items;
 
 }
